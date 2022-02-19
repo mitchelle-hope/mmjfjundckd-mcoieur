cdf3578d10d27c75e1fcaa265c8836631e825ee6
mitchelle-hope <mitchellehopewaithera@gmail.com>
2022-02-19 07:50:12 +0300

    first commit

---
+++ src/main.kt
@@ -0,0 +1,27 @@
+//question one
+fun main(){
+    myname()
+    modulus(9,2)
+    add(6,9,7,3)
+    interestingfact()
+}
+fun myname(){
+    var name=("hello " + "Mitchell")
+    println(name)
+    }
+//question two
+fun modulus(s:Int,r:Int){
+    var modulus = s%r
+    println(modulus)
+}
+//question 3
+fun add(a:Int,b:Int,c:Int,d:Int){
+    var sum=(a+b+c+d)
+    println(sum)
+}
+// question 3
+fun interestingfact(){
+    var funfact="I love travelling  "
+println(funfact)
+
+}
